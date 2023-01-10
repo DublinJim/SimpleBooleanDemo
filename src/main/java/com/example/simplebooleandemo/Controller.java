420526a4aadd2ce9c21c4eaee8206d4df94b7203
DublinJim <59180380+DublinJim@users.noreply.github.com>
2023-01-10 10:54:16 -0500

    add the initial check

--- src/main/java/com/example/simplebooleandemo/Controller.java
+++ src/main/java/com/example/simplebooleandemo/Controller.java
@@ -1,5 +1,6 @@
 package com.example.simplebooleandemo;
 
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.fxml.Initializable;
 import javafx.scene.control.CheckBox;
 import javafx.scene.layout.AnchorPane;
@@ -13,9 +14,16 @@ public class Controller implements Initializable {
     public AnchorPane rootPane;
     public CheckBox chk1;
     public Text txt1;
+    private SimpleBooleanProperty simpleBooleanProperty = new SimpleBooleanProperty();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        System.out.println("Before checking checkbox "+simpleBooleanProperty);
+        if (chk1.isSelected()) {
+            simpleBooleanProperty.setValue(chk1.isSelected());
+        }
+        System.out.println("After the check " + simpleBooleanProperty);
     }
+
+
 }
