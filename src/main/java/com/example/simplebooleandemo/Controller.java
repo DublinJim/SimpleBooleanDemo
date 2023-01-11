e20ac78dd70bfd71a24257aebd4a2de50014d91a
DublinJim <59180380+DublinJim@users.noreply.github.com>
2023-01-11 14:22:09 -0500

    added graphics of a coin

--- src/main/java/com/example/simplebooleandemo/Controller.java
+++ src/main/java/com/example/simplebooleandemo/Controller.java
@@ -1,11 +1,10 @@
 package com.example.simplebooleandemo;
 
 import javafx.beans.property.SimpleBooleanProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.CheckBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 
@@ -14,20 +13,19 @@ import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
+    private final SimpleBooleanProperty simpleBooleanProperty = new SimpleBooleanProperty();
     public AnchorPane rootPane;
     public CheckBox chk1;
     public Text txt1;
-    private SimpleBooleanProperty simpleBooleanProperty = new SimpleBooleanProperty();
+    public ImageView imageView;
+    private Image image1;
+    private Image image2;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-        simpleBooleanProperty.addListener(new ChangeListener<Boolean>() {
-            @Override
-            public void changed(ObservableValue<? extends Boolean> observableValue, Boolean aBoolean, Boolean t1) {
-                setNewText();
-            }
-        });
+        /* simpleBooleanProperty.addListener((observableValue, aBoolean, t1) -> setNewText());*/
+        simpleBooleanProperty.addListener((observable -> setNewText()));
 
         System.out.println("Before checking checkbox " + simpleBooleanProperty);
         txt1.setText("After the check " + simpleBooleanProperty.getValue());
@@ -38,14 +36,23 @@ public class Controller implements Initializable {
         txt1.setText("After the check " + simpleBooleanProperty.getValue());
 
 
+        image1 = new Image("C:\\Users\\james.keogh\\IdeaProjects\\proto\\SimpleBooleanDemo\\src\\main\\resources\\Images\\coin-teaser-v2.jpg");
+        image2 = new Image("C:\\Users\\james.keogh\\IdeaProjects\\proto\\SimpleBooleanDemo\\src\\main\\resources\\Images\\Royal_Canadian_Mint_The_Royal_Canadian_Mint_Launches_its_Largest.jpg");
+
     }
 
     public void setNewText() {
         txt1.setText(simpleBooleanProperty.toString());
     }
 
-    public void checkBoxHandler(ActionEvent actionEvent) {
-        simpleBooleanProperty.setValue(chk1.isSelected());
+    public void checkBoxHandler() {
+        if (chk1.isSelected()) {
+            simpleBooleanProperty.setValue(chk1.isSelected());
+            imageView.setImage(image1);
+        }
+        else {
+            imageView.setImage(image2);
+        }
 
     }
 }
